eeac369f8ec77d522883488a2d72ac9472c169ca
Therealbosse <vilgotros@gmail.com>
2024-09-02 14:21:10 +0200

    Add files via upload

---
+++ bossescripts-jobcenter/config.lua
@@ -0,0 +1,57 @@
+Config = {}
+
+Config.Locations = {
+    {
+        position = vector3(268.5521, -741.2452, 39.0417),
+        jobs = {
+            {
+                name = "taxi",
+                label = "Maxi AB",
+                description = "Vill du jobba? Tjäna snabba pengar. Börja då jobba här för en enklare ekonomi!",
+                icon = "mdi-taxi"
+            },
+            {
+                name = "police",
+                label = "Laxi AB",
+                description = "Ta stadens brottslingar? Gripa kriminella som gör samhället osäkert?",
+                icon = "mdi-clock-fast"
+            },
+            {
+                name = "taxi",
+                label = "Kaxi AB",
+                description = "Jobba för stadens godaste restuarang? Anslut dig idag!",
+                icon = "mdi-food"
+            },
+            {
+                name = "garbage",
+                label = "Baxi AB",
+                description = "Köra bil eller dricka vin? Vad väljer du? (VIN)",
+                icon = "mdi-beer"
+            },
+            {
+                name = "taxi",
+                label = "Loxi AB",
+                description = "Vara en stor pojk eller en liten pojk? Jadu jag  undrar...",
+                icon = "mdi-coffee"
+            },
+            {
+                name = "garbage",
+                label = "Poxy AB",
+                description = "Jobba för att få till samhället? Anslut dig idag, vi kommer ge en utbildning i början!",
+                icon = "mdi-server-off"
+            },
+            {
+                name = "taxi",
+                label = "Enro AB",
+                description = "Brum brum brum, varför låter det så mycket? Min mickro BRINNER! (Ojsan)",
+                icon = "mdi-timetable"
+            },
+            {
+                name = "garbage",
+                label = "Cata AB",
+                description = "Åh nej... Katten är i trädet!? Tur att vi har bästa räddningstjänsten i denna stad!",
+                icon = "mdi-pine-tree"
+            },
+        }
+    }
+}
